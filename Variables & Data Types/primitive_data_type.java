1b9bac57a4064a73f44d23db5805278c46859d14
Sanoj Yadav <sanojyadav2700@gmail.com>
2026-02-03 01:01:48 +0530

    add Variables & Data Types folder

---
+++ Variables & Data Types/primitive_data_type.java
@@ -0,0 +1,35 @@
+public class primitive_data_type {
+    public static void main(String args[]) {
+        // byte example
+        byte byteVar = 100;
+        System.out.println("byte: " + byteVar);
+
+        // short example
+        short shortVar = 20000;
+        System.out.println("short: " + shortVar);
+
+        // int example
+        int intVar = 123456;
+        System.out.println("int: " + intVar);
+
+        // long example
+        long longVar = 12345678900L;
+        System.out.println("long: " + longVar);
+
+        // float example
+        float floatVar = 3.14f;
+        System.out.println("float: " + floatVar);
+
+        // double example
+        double doubleVar = 3.14159265359;
+        System.out.println("double: " + doubleVar);
+
+        // char example
+        char charVar = 'A';
+        System.out.println("char: " + charVar);
+
+        // boolean example
+        boolean boolVar = true;
+        System.out.println("boolean: " + boolVar);
+    }
+}
---
+++ Variables & Data Types/sum_of_two_numbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class sum_of_two_numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("Sum of two numbers = " + sum);
+
+        sc.close();
+    }
+}
