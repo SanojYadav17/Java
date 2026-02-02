6ae307a2357014ccc2104d4bb320dd4c3f032191
Sanoj Yadav <sanojyadav2700@gmail.com>
2026-02-03 03:25:08 +0530

    Added Input_in_java.java for user input example

---
+++ Variables & Data Types/Input_in_java.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Input_in_java {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Name: " + name);
+
+        String input = sc.nextLine();
+        System.out.println("Input: " + input);
+
+        int number = sc.nextInt();
+        System.out.println("Number: " + number);
+
+        int num  = sc.nextByte();
+        System.out.println("Num: " + num);
+
+        float num1 = sc.nextFloat();
+        System.out.println("Float: " + num1);
+
+        double nam2 = sc.nextDouble();
+        System.out.println("Double: " + nam2);
+
+        Boolean name1Boolean = sc.nextBoolean();
+        System.out.println("Name: " + name1Boolean);
+
+        short num3 = sc.nextShort();
+        System.out.println("Short: " + num3);
+
+        long num4 = sc.nextLong();
+        System.out.println("Long: " + num4);
+        
+        sc.close();
+    }
+}
