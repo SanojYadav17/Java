4e244814f69af58540743b76e43bc4b0bcd9bcae
Sanoj Yadav <150279689+SanojYadav17@users.noreply.github.com>
2026-02-05 00:06:27 +0530

    Uncomment IfElse class and main method

--- Conditional Statement/IfElse.java
+++ Conditional Statement/IfElse.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 // If_Else Statement without user input
 
-// public class IfElse {
-    //     public static void main(String[] args) {
-    //         int age = 22;
-    //         if (age >= 18) {
-    //             System.out.println("You are Eligible For Voting, Driving.");
-    //         }
-    //         else {
-    //             System.out.println("You are Not Eligible For Voting, Driving.");
-    //         }
-    //     }	
-    // }
+public class IfElse {
+        public static void main(String[] args) {
+            int age = 22;
+            if (age >= 18) {
+                System.out.println("You are Eligible For Voting, Driving.");
+            }
+            else {
+                System.out.println("You are Not Eligible For Voting, Driving.");
+            }
+        }	
+    }
 
     // If_Else Statement with user input
 public class IfElse {
