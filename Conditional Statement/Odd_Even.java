fb91174adc54d19cae6c696e01b1931631509760
Sanoj Yadav <sanojyadav2700@gmail.com>
2026-02-05 02:03:07 +0530

    Add Odd_Even program and update VS Code settings

---
+++ Conditional Statement/Odd_Even.java
@@ -0,0 +1,31 @@
+// Program to check if a number is odd or even without user input
+
+// public class Odd_Even {
+//     public static void main(String[] args) {
+//         int a = 7;
+//         if (a % 2 == 0) {
+//             System.out.println("The number is even.");
+//         } else {
+//             System.out.println("The number is odd.");
+//         }
+//     }
+// }
+
+
+// Program to check if a number is odd or even with user input
+
+import java.util.*;
+
+public class Odd_Even {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int a = sc.nextInt();
+
+        if (a % 2 == 0) {
+            System.err.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");   
+        }
+    }
+};
