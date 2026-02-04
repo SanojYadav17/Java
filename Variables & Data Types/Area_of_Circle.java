edda4573e38b348559e29b5e6a2cf452e5ee58e2
Sanoj Yadav <sanojyadav2700@gmail.com>
2026-02-04 22:51:47 +0530

    Update relational operator and area of circle programs

--- Variables & Data Types/Area_of_Circle.java
+++ Variables & Data Types/Area_of_Circle.java
@@ -1,7 +1,5 @@
-
-import java.util.Scanner;
-
 //  print area of circle using user input
+import java.util.Scanner;
 public class Area_of_Circle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
