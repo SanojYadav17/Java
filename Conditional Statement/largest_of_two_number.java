4c37184efbaf1e7443737f111aadba3b279ac7f8
Sanoj Yadav <150279689+SanojYadav17@users.noreply.github.com>
2026-02-05 02:30:16 +0530

    Enable functionality to find largest of two numbers
    Uncommented the code to find the largest of two numbers.

--- Conditional Statement/largest_of_two_number.java
+++ Conditional Statement/largest_of_two_number.java
@@ -1,18 +1,18 @@
 // largest of two number without user input
 
-// public class largest_of_two_number {
-//     public static void main (String [] args){
-//         int a =20;
-//         int b = 41;
+public class largest_of_two_number {
+    public static void main (String [] args){
+        int a =20;
+        int b = 41;
 
-//         if (a>=b){
-//             System.out.println(a + " A is largest number");
-//         }
-//         else{
-//             System.out.println(b + " B is largest number");
-//         }
-//     }
-// }
+        if (a>=b){
+            System.out.println(a + " A is largest number");
+        }
+        else{
+            System.out.println(b + " B is largest number");
+        }
+    }
+}
 
 // largest of two number with user input4
 
