5c40a3630ef1abc61bf7bd0a379c36a8d256adf5
Sanoj Yadav <sanojyadav2700@gmail.com>
2026-02-08 02:42:26 +0530

    add income_tax_calculator

---
+++ Conditional Statement/income_tax_calculator.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class income_tax_calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your annual income: ");
+
+        double income = sc.nextDouble();
+
+        if (income < 500000) {
+            System.out.println("No tax applicable.");
+        }
+        else if (income>=500000 && income <1000000){
+
+            double tax = income * 0.2;
+            System.out.println("Your tax is: " + tax);
+        }
+        else if (income>= 1000000){
+            double tax = income * 0.3;
+            System.out.println("Your tax is: " + tax);
+        }
+    }
+}
