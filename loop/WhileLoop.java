981b88ea74bfcf9597640b587a1a9cc8a069d961
Sanoj Yadav <sanojyadav2700@gmail.com>
2026-03-04 01:18:14 +0530

    Refactor settings and uncomment switch statement examples

---
+++ loop/WhileLoop.java
@@ -0,0 +1,29 @@
+// print "Hello world" 100 times using while loop
+
+package loop;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 100) {
+            System.out.println("Hello world");
+            count++;
+        }
+    }
+}
+
+
+// print number from 1 to 10 using while loop 
+
+package loop;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num <= 10) {
+            System.out.println("Number: " + num);
+            num++;
+        }
+    }
+}
