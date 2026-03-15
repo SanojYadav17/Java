b35a8e46092d0a942169743cd392d80d2fa1d357
Sanoj Yadav <150279689+SanojYadav17@users.noreply.github.com>
2026-03-16 00:24:07 +0530

    For Loop

--- loop/ForLoop.java
+++ loop/ForLoop.java
@@ -1,52 +1,51 @@
 // Print "Hello world" 10 times using a for loop
 
-// package loop;
-// public class ForLoop {
-//     public static void main(String[] args) {
-//         for (int i = 0; i < 10; i++) {
-//             System.out.println("Hello world");
-//         }
-//     }
-// }
+package loop;
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello world");
+        }
+    }
+}
 
 // print Squre pattern using for loop
 
-// package loop;
-// public class ForLoop {
-//     public static void main(String[] args) {
-//         for (int i = 0; i < 4; i++) {
-//             System.out.println("*****");
-//         }   
-//     }
-// }
+package loop;
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            System.out.println("*****");
+        }   
+    }
+}
 
 // reverse of given number using for loop
 
-// package loop;
-// public class ForLoop {
-//     public static void main(String[] args) {
-//         int num = 12045;
-//         int reverse = 0;
+package loop;
+public class ForLoop {
+    public static void main(String[] args) {
+        int num = 12045;
+        int reverse = 0;
 
-//         for (int i = num; i > 0; i /= 10) {
-//             int digit = i % 10;
-//             reverse = reverse * 10 + digit;
-//         }
-
-//         System.out.println("Reverse of " + num + " is: " + reverse);
-//     }
-// }
+        for (int i = num; i > 0; i /= 10) {
+            int digit = i % 10;
+            reverse = reverse * 10 + digit;
+        }
+        System.out.println("Reverse of " + num + " is: " + reverse);
+    }
+}
 
 // print reverse of given number using while loop
 
-// package loop;
-// public class ForLoop {
-//     public static void main(String[] args) {
-//         int n = 10899;
-//         while (n > 0) {
-//             int digit = n % 10;
-//             System.out.print(digit);
-//             n /= 10;
-//         }
-//     }
-// }
+package loop;
+public class ForLoop {
+    public static void main(String[] args) {
+        int n = 10899;
+        while (n > 0) {
+            int digit = n % 10;
+            System.out.print(digit);
+            n /= 10;
+        }
+    }
+}
