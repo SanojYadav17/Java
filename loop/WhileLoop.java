861eaf41142eb5d4c150457ca741b7d93a3a58c2
Sanoj Yadav <150279689+SanojYadav17@users.noreply.github.com>
2026-03-16 00:25:19 +0530

    While Loop

--- loop/WhileLoop.java
+++ loop/WhileLoop.java
@@ -1,82 +1,80 @@
 // // print "Hello world" 100 times using while loop
 
-// package loop;
-// public class WhileLoop {
-//     public static void main(String[] args) {
-//         int count = 0;
+package loop;
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
 
-//         while (count < 100) {
-//             System.out.println("Hello world");
-//             count++;
-//         }
-//     }
-// } 
+        while (count < 100) {
+            System.out.println("Hello world");
+            count++;
+        }
+    }
+} 
 
 
 // // print number from 1 to 10 using while loop 
 
-// package loop;
-// public class WhileLoop {
-//     public static void main(String[] args) {
-//         int num = 1;
-//         while (num <= 10) {
-//             System.out.println("Number: " + num);
-//             num++;
-//         }
-//     }
-// }
+package loop;
+public class WhileLoop {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num <= 10) {
+            System.out.println("Number: " + num);
+            num++;
+        }
+    }
+}
 
 // // print number from 1 to n using while loop
 
-// package loop;
-// import java.util.Scanner;
+package loop;
+import java.util.Scanner;
+public class WhileLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
 
-// public class WhileLoop {
-//     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
-//         System.out.print("Enter a number: ");
-//         int n = scanner.nextInt();
-
-//         int num = 1;
-//         while (num <= n) {
-//             System.out.print(num + " ");
-//             num++;
-//         }
-//         System.out.println();
-//     }
-    
-// }
+        int num = 1;
+        while (num <= n) {
+            System.out.print(num + " ");
+            num++;
+        }
+        System.out.println();
+    }  
+}
 
 // // print sum of first n natural numbers using while loop
 
-// package loop;
-// import java.util.Scanner;  
-// public class WhileLoop {
-//     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
-//         System.out.print("Enter a number: ");
-//         int n = scanner.nextInt();
+package loop;
+import java.util.Scanner;  
+public class WhileLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
 
-//         int sum = 0;
-//         int num = 1;
-//         while (num <= n) {
-//             sum += num;
-//             num++;
-//         }
-//         System.out.println("Sum of first " + n + " natural numbers is: " + sum);
-//         scanner.close();
-//     }
-// }
+        int sum = 0;
+        int num = 1;
+        while (num <= n) {
+            sum += num;
+            num++;
+        }
+        System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+        scanner.close();
+    }
+}
 
 // print Squre pattern using while loop
 
-// package loop;
-// public class WhileLoop {
-//     public static void main(String[] args) {
-//         int i = 0;
-//         while (i < 4) {
-//             System.out.println("*****");
-//             i++;
-//         }
-//     }
-// }   
+package loop;
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 4) {
+            System.out.println("*****");
+            i++;
+        }
+    }
+}   
