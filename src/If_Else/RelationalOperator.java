2835c849a066c0d1b656ac706e14399f0476b81f
Sanoj Yadav <sanojyadav2700@gmail.com>
2026-08-23 16:38:01 +0530

    Basics

---
+++ src/If_Else/RelationalOperator.java
@@ -0,0 +1,15 @@
+package If_Else;
+
+public class RelationalOperator {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a == b); // false
+        System.out.println(a != b); // true
+        System.out.println(a > b);  // false
+        System.out.println(a < b);  // true
+        System.out.println(a >= b); // false
+        System.out.println(a <= b); // true
+    }
+}
