fc66abd4d9e159096207034bf27fe862974b9875
Sanoj Yadav <sanojyadav2700@gmail.com>
2026-08-23 16:09:42 +0530

    Basics

--- src/Basics/CharDataType.java
+++ src/Basics/CharDataType.java
@@ -2,7 +2,40 @@ package Basics;
 
 public class CharDataType {
     public static void main(String[] args) {
-        char sanoj = 'S';
-        System.out.println(sanoj);
+        Character ch ='S';
+        System.out.println(ch);
+
+        Character Manoj ='M';
+        System.out.println(Manoj);
+
+        Character PP ='7';
+        System.out.println(PP);
+
+//  typecasting - ek data type se doosra data type conversion pe jana
+
+        char ch3 = 'A';
+        int x = ch3; // implicit typecasting
+        System.out.println(x);
+
+        char ch1 = 'a';
+        int x1 = (int)ch1;  // explicit typecasting
+        System.out.println(x1);
+
+        char ch2 = '4';
+        System.out.println((int)ch2);
+
+        char ch6 = 'c';
+        System.out.println(ch6);
+
+        char ch4 = 'c';
+        System.out.println(ch4 * 2);
+
+        char ch5 = 'c';
+        System.out.println(ch5 + 2);
+
+//        integer to character
+        int x2 = 99;
+        char ch7 = (char)x2;
+        System.out.println(ch7);
     }
 }
---
+++ src/Basics/IncrementDecrement.java
@@ -0,0 +1,35 @@
+package Basics;
+
+public class IncrementDecrement {
+    public static void main(String[] args){
+//        int x = 10;
+//        System.out.println(x);
+//        x +=6;
+//        System.out.println(x);
+//        x *=4;
+//        System.out.println(x);
+//        x -= 5;
+//        System.out.println(x);
+//
+        int y =24;
+        System.out.println(y++);
+        System.out.println(y);
+
+        int z = 28;
+        System.out.println(++z);
+        System.out.println(z);
+
+        int a = 10;
+        System.out.println(a--);
+        System.out.println(a);
+
+        int b = 10;
+        System.out.println(--b);
+        System.out.println(b);
+
+        int c = 10;
+        int d = c++; // y = 10 x = 11
+        System.out.println(c+"  "+d);
+
+    }
+}
