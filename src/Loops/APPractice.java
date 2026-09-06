d0183a458d3f0f042f156463c2c45691dd46fec6
Sanoj Yadav <sanojyadav2700@gmail.com>
2026-09-06 15:43:45 +0530

    Loops

---
+++ src/Loops/APPractice.java
@@ -0,0 +1,15 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class APPractice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+//        int n = sc.nextInt();
+        for (int i=99; i>=0; i-=4){
+            System.out.println(i);
+        }
+    }
+}
